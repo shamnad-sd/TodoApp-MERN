94b47e02dcd9687fe1905d6640d7358fcaacd930
SdCode000 <shamnad2nk@gmail.com>
2024-12-31 18:16:07 +0530

    MERN PROJECT

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import Create from './Create'
 import axios from 'axios'
+import { BsCircleFill, BsFillTrashFill } from 'react-icons/bs'
 
 const Home = () => {
 
@@ -12,6 +13,12 @@ const Home = () => {
     .then(result => setTodos(result.data))
     .catch(error => console.log(error))
   },[])
+
+  const handleEdit = (id) => {
+    axios.put('http://localhost:3001/update/'+id)
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+  }
   
   return (
     <div className='Header'>
@@ -21,9 +28,11 @@ const Home = () => {
         todos.length === 0 ? 
         <div><h2>No Todo</h2></div>
         :
-        todos.map((todo,index) =>(
-          <div key={index}>
+        todos.map((todo) =>(
+          <div>
+            <BsCircleFill onClick={() => handleEdit(todo._id)}/>
             {todo.task}
+            <BsFillTrashFill/>
           </div>
         ))
       }
